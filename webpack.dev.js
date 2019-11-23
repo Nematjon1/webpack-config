1f143d4e79d4f09ead2618c9e667d674e9392da6
Nematjon <frank.sport.93@gmail.com>
2019-11-23 16:12:37 +0300

    Added new rules and loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,11 +10,71 @@ module.exports = merge(common, {
   },
   module: {
     rules: [
+           {
+        test: /\.css$/,
+        use: [
+          // style loader
+          {loader: "style-loader"},
+          // css loader
+          {
+            loader: "css-loader",
+            modules: true
+          },
+          // cass loader
+          {loader: "sass-loader"}
+        ]
+      },
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        loader: "babel-loader"
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/,
+        use: [
+          "file-loader"
+        ]
+      },
+      {
+        test: /\.(woff|ttf|eot|woff2|otf)$/,
+        use: [
+          "file-loader"
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          "csv-loader"
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          "xml-loader"
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true
+            }
+          }
+        ]
+      },
+      {
+        test: require.resolve("index.js"),
+        use: "imports-loader?this=>window"
       }
+
     ]
   }
 });
